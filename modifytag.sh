c5490022b7c396cd001e65b07a9e5dcc73c9a17a
Ramesh Puttaganti <rpcldops@gmail.com>
2020-07-04 17:25:47 +0000

    modified

--- modifytag.sh
+++ modifytag.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#sed "s/tagstring/$1/g" blue-temp.yml > deploy.yml
-sed "s/tagstring/$1/g" green-temp.yml > deploy.yml
+sed "s/tagstring/$1/g" blue-temp.yml > deploy.yml
+#sed "s/tagstring/$1/g" green-temp.yml > deploy.yml
